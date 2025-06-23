ccc8fbe53ab3641313772fb41a01f51873222e52
Karthi <142215264+kaarthii@users.noreply.github.com>
2025-06-23 11:32:06 +0530

    Update textToSpeech.py

--- voice/textToSpeech.py
+++ voice/textToSpeech.py
@@ -1,9 +1,20 @@
-import pyttsx3
-import whisper
-import torch
+from gtts import gTTS
+from pydub import AudioSegment 
+import os
 
 
 def speak(text):
-    engine=pyttsx3.init()
-    engine.say(text)
-    engine.runAndWait()
+    temp_mp3="temp_audio.mp3"
+    file_name="ai_response.wav"
+
+    tts=gTTS(text=text,lang='en')
+    tts.save(temp_mp3)
+
+    sound=AudioSegment.from_mp3(temp_mp3)
+    sound.export(file_name,format="wav")
+
+    os.remove(temp_mp3)
+
+    print(f"Ai response is saved as {file_name}")
+    return file_name
+
